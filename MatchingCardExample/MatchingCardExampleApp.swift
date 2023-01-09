2a22d617c03f372b4b620f8922670d671979f3fb
katafuchi <katafuchix@gmail.com>
2023-01-09 18:18:59 +0900

    Initial Commit

---
+++ MatchingCardExample/MatchingCardExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingCardExampleApp.swift
+//  MatchingCardExample
+//
+//  Created by cano on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct MatchingCardExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
